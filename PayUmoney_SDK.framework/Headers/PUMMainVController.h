7f50a1b4c19f21b758f48407fa7c876253e0449a
honey-lakhani <honey.lakhani@payu.in>
2017-01-19 13:37:09 +0530

    framework file added

---
+++ PayUmoney_SDK.framework/Headers/PUMMainVController.h
@@ -0,0 +1,12 @@
+//
+//  PUMMainVController.h
+//  PayUmoneyiOS_SDK
+//
+//  Created by Imran Khan on 7/21/16.
+//  Copyright © 2016 PayUmoney. All rights reserved.
+//
+
+#import "PUMBaseVC.h"
+
+@interface PUMMainVController : PUMBaseVC
+@end
