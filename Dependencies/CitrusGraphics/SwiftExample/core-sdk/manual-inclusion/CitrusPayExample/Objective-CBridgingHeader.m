aba8145ebf079791decd7cf085d50684362e985f
Umang Arya <umang.arya@payu.in>
2018-01-11 13:49:30 +0530

    removed submodule directory

--- Dependencies/CitrusGraphics/SwiftExample/core-sdk/manual-inclusion/CitrusPayExample/Objective-CBridgingHeader.m
+++
@@ -1,9 +0,0 @@
-//
-//  Objective-CBridgingHeader.m
-//  CitrusPayExample
-//
-//  Created by Mukesh Patil on 4/19/16.
-//  Copyright © 2016 CitrusPay. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
